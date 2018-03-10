c9f41bdbaca27e95e176dc4e177d7b944b682c46
yuureilynh <37130656+yuureilynh@users.noreply.github.com>
2018-03-10 22:04:55 +0700

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,9 @@
+
+public class ex1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Nguyen Le Thi Bao Linh");
+	}
+
+}
